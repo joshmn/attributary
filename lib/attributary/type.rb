5b0cfe2fcca9c87e870839c3552ae2acadf085ce
Josh Brody <josh@josh.mn>
2018-06-18 19:43:10 -0500

    remove all those requires

--- lib/attributary/type.rb
+++ lib/attributary/type.rb
@@ -7,3 +7,12 @@ module Attributary
     end
   end
 end
+
+require 'attributary/types/array_type'
+require 'attributary/types/big_decimal_type'
+require 'attributary/types/boolean_type'
+require 'attributary/types/float_type'
+require 'attributary/types/hash_type'
+require 'attributary/types/integer_type'
+require 'attributary/types/string_type'
+require 'attributary/types/symbol_type'
